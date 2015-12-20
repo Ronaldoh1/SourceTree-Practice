cf18e121c9daf7fc3d91742b453e8488ee98ea70
Ronald Hernandez <rhernandez@solstice-mobile.com>
2015-12-20 12:23:59 -0600

    Initial Commit

---
+++ SourTreePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SourTreePractice
+//
+//  Created by Ronald Hernandez on 12/20/15.
+//  Copyright © 2015 Ronald Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
